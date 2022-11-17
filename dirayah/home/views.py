935f4c170bcec085194b1d3a38ce1fbec53ee975
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 19:31:45 +0200

    added home page elements

--- dirayah/home/views.py
+++ dirayah/home/views.py
@@ -6,12 +6,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib import messages
 from .forms import NewUserForm, LoginForm
-from .models import UserProfile
+from .models import UserProfile, User
 
 # Create your views here.
 
 # Home Page
 def index(request):
+    if request.user.is_authenticated:
+        answers=UserProfile.objects.get(user=request.user).answers
+        recommendations=UserProfile.objects.get(user=request.user).recommendations
+        return render(request, 'home/index.html',
+        {
+            "answers":answers,
+            "recommendations":recommendations,
+        }
+        )
     return render(request, 'home/index.html')
 
 # Authenticate user on login
@@ -34,13 +43,13 @@ def login_view(request):
             login(request, user)
             return JsonResponse({
                 "message": "success"
-            })
+            }) 
         else:
             return JsonResponse({
                 "message": "invalid credentials"
             })
     else:
-        return render(request, "home/login.html", {"login_form": LoginForm()})
+        return render(request, "home/login.html", {"login_form": form})
 
 def register_view(request):
     if request.user.is_authenticated:
@@ -54,7 +63,7 @@ def register_view(request):
             messages.success(request, "Registration successful." )
             return redirect("index")
         messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
+    form = NewUserForm() 
     return render (request=request, template_name="home/register.html", context={"register_form":form})
 
 def logout_view(request):
